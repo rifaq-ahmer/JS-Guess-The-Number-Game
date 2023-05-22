f6820331fb5518dbebc9fdcc4721307a33916beb
rifaq-ahmer <rifaq.ahmer@gmail.com>
2023-05-22 23:46:07 +0530

    implemented play Again bitton functionality

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 'use strict';
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
   if (!guess) {
     document.querySelector('.message').textContent =
       'Please Enter any Number 🥲';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number..!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too High...!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High...!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        '💥 You Lost The Game..!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too Low...!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low...!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        '💥 You Lost The Game..!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
