03bfc6ac60c9e3d2d01c9fdf1e915a602ed81529
rifaq-ahmer <rifaq.ahmer@gmail.com>
2023-05-22 23:14:09 +0530

    implemented guess number and number to be searched logic

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 'use strict';
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
   if (!guess) {
     document.querySelector('.message').textContent =
       'Please Enter any Number 🥲';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number..!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too High...!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too Low...!';
   }
 });
